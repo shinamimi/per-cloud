1582923f11da83f91127a4d40371aa5dc6898963
shinamimi <1018515982@qq.com>
2026-07-25 03:34:29 +0800

    chore: initialize backend infrastructure

---
+++ backend/src/main/java/com/cloud/backend/CloudBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudBackendApplication.class, args);
+	}
+
+}
